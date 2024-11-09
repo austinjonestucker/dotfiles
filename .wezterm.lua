b99fc06750c7c7ac47c7c5bf1003953649e43920
Austin Tucker <austin.jones.tucker@gmail.com>
2024-11-09 12:39:29 -0500

    edit wezterm.lua for multi-os comp

--- .wezterm.lua
+++ .wezterm.lua
@@ -16,7 +16,6 @@ wezterm.on('gui-startup', function()
  window:gui_window():maximize()
 end)
 
-
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
@@ -62,6 +61,8 @@ config.keys = {
     action = act.SendKey { key = 'u', mods = 'CTRL' },
   },
 }
+
+-- Window padding
 config.window_padding = {
   left = '0',
   right = '0',
@@ -72,25 +73,29 @@ config.window_padding = {
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
   -- Requires WSL & Ubuntu to be installed
   config.default_domain = 'WSL:Ubuntu'
+  config.font_size = 11
 else
-  -- Windows has font issues, so only change font in *nix
-  config.font = wezterm.font('MesloLGS NF')
+  -- Tabs not needed in *nix with tmux & only one terminal
+  config.hide_tab_bar_if_only_one_tab = true
+  config.font_size = 13
 end
 
--- This is where you actually apply your config choices
+-- Editor styling
 config.color_scheme = 'Kanagawa (Gogh)'
-config.font_size = 13
+config.font = wezterm.font('MesloLGS NF')
 config.line_height = 0.925
 config.default_cursor_style = 'BlinkingBlock'
 config.cursor_blink_rate = 500
 config.cursor_blink_ease_in = "Constant"
 config.cursor_blink_ease_out = "Constant"
-config.use_dead_keys = false
-config.scrollback_lines = 10000
+
+-- Terminal styling
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = 'RESIZE'
 config.native_macos_fullscreen_mode = false
 
--- and finally, return the configuration to wezterm
-return config
+-- Other configs
+config.use_dead_keys = false
+config.scrollback_lines = 10000
 
+return config
