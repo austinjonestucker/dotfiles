c65700f5fe1af610de668173619d7cce9e63dcde
Austin Tucker <austin.jones.tucker@gmail.com>
2024-11-13 16:27:29 -0500

    add visual search to be default search in neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -74,6 +74,7 @@ vim.o.relativenumber = true
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set({'n', 'v'}, '/', "/\\v", { noremap = true, silent = true })
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
