804870e8919ad4b9567dea27108229feefd902e0
Austin Tucker <austin.jones.tucker@gmail.com>
2025-02-22 22:15:22 -0500

    update devpod setup to move starship to correct location

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ mkdir -p $XDG_CONFIG_HOME/nixpkgs
 ln -sf $PWD/.config/nvim $XDG_CONFIG_HOME/nvim
 ln -sf $PWD/.config/neofetch $XDG_CONFIG_HOME/neofetch
 ln -sf $PWD/.bashrc $HOME/.bashrc
-ln -sf $PWD/.config/starship.toml $HOME/starship.toml
+ln -sf $PWD/.config/starship.toml $XDG_CONFIG_HOME/starship.toml
 ln -sf $PWD/config.nix $XDG_CONFIG_HOME/nixpkgs/config.nix
 
 nix-env -iA nixpkgs.myPackages
